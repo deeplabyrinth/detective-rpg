8be327133bcf5ed1027667cc5ea45c18238adad5
John Quiruz <jquiruz1@gmail.com>
2025-08-26 17:44:33 -0700

    create a window

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //TODO: Create a window
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Detective RPG");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
