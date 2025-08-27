706f71a386799021cb53939d5262fea318769567
John Quiruz <jquiruz1@gmail.com>
2025-08-26 20:08:35 -0700

    wire key handler into game panel and add delta/accumulator game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,14 +15,26 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // FPS
+    int FPS = 60;
+
+    //TODO: Creates a key handler for keyboard input
+    KeyHandler keyH = new KeyHandler();
     //TODO: Construct a game clock
     Thread gameThread;
+
+    //TODO: Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
 
@@ -32,22 +44,87 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start(); // call run method below
     }
 
-    @Override
+//    @Override
+//    public void run() {
+//
+//        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        // When we create a Thread object, the object automatically calls this run method.
+//        while (gameThread != null) {
+//
+//            long currentTime = System.nanoTime();
+//            System.out.println("Current time: " + currentTime);
+//
+//            // 1 UPDATE: update information such as character positions
+//            update();
+//
+//            // 2 DRAW: draw the screen with the updated information
+//            repaint(); // call paint Component method
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime / 1000000;
+//
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
     public void run() {
-        // When we create a Thread object, the object automatically calls this run method.
+
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
         while (gameThread != null) {
 
-//            System.out.println("The game loop is running");
+            currentTime = System.nanoTime();
 
-            // 1 UPDATE: update information such as character positions
-            update();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-            // 2 DRAW: draw the screen with the updated information
-            repaint(); // call paint Compoent method
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
         }
     }
 
-    public void update() {}
+    public void update() {
+
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+
+    }
 
     public void paintComponent(Graphics g) {
 
@@ -57,7 +134,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
